ead967732ae9218b6a1e09426ca69f11f8fe768f
Spynocturne <hugodolat@gmail.com>
2026-04-16 15:26:13 +0200

    insertion dans la BDD

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/config/database.php';
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") { /*si formulaire envoyer*/
 
     $pseudo = $_POST['pseudo'];
     $email = $_POST['email'];
@@ -11,6 +11,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $sql = "INSERT INTO users (pseudo, email) VALUES ('$pseudo', '$email')";
         $pdo->exec($sql);
 
+        /*redirection*/
+        header("Location: index.php");
+        exit;   
+
         echo "Utilisateur ajouté !";
 
     } catch (PDOException $e) {
@@ -18,22 +22,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 
-/* Partie connexion */
+/* Partie affichage */
 
 include __DIR__ . '/includes/header.php'; /*__DIR__  permet de donner le chemin absolue */
 include __DIR__ . '/pages/accueil.php'; 
 include __DIR__ . '/includes/footer.php';
 /* require possible à la place de include */
-
-require __DIR__ . '/config/database.php';
-
-try {
-    $sql = "INSERT INTO users (pseudo, email) VALUES ('Hugo', 'hugo@test.com')";
-    
-    $pdo->exec($sql);
-
-    echo "Utilisateur ajouté !";
-
-} catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-}
